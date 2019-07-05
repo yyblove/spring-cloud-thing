cd9f3c7593081ff46c4f853c33fb06d4846b3153
yyblove <yyblove@yeah.net>
2019-07-05 17:27:21 +0800

    高可用的网关

--- gateway-server/src/main/java/thing/gateway/server/GatewayServerApplication.java
+++ gateway-server/src/main/java/thing/gateway/server/GatewayServerApplication.java
@@ -2,13 +2,15 @@ package thing.gateway.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.bind.annotation.RestController;
 
 
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayServerApplication {
 
@@ -22,7 +24,7 @@ public class GatewayServerApplication {
         return builder.routes()
                 .route(p -> p.path("/get")
                         .filters(f -> f.addRequestHeader("hello", "yyb")
-                        .preserveHostHeader())
+                                .preserveHostHeader())
                         .uri("http://httpbin.org"))
                 .build();
     }
